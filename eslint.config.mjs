3d0f27458f16d5e249ad0b0d64672426cce77dc6
AshedFox <a.mahn@mail.ru>
2025-03-28 19:36:19 +0300

    add eslint import module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,8 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginImportX from 'eslint-plugin-import-x';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,14 +19,24 @@ const compat = new FlatCompat({
 });
 
 export default defineConfig([
-  globalIgnores(['**/.eslintrc.js']),
+  eslintPluginImportX.flatConfigs.recommended,
+  eslintPluginImportX.flatConfigs.typescript,
+  globalIgnores(['eslint.config.mjs']),
   {
+    files: ['{src,apps,libs,test}/**/*.ts'],
     extends: compat.extends(
       'plugin:@typescript-eslint/recommended',
       'plugin:prettier/recommended',
     ),
     plugins: {
       '@typescript-eslint': typescriptEslintEslintPlugin,
+    },
+    settings: {
+      'import-x/resolver-next': [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+        }),
+      ],
     },
     languageOptions: {
       globals: {
